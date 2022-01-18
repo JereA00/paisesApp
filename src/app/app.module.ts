d58daa8c97bb7e222ee6ecd95252c74e3eaf9d85
JereA00 <jeremias_davidenco@hotmail.com>
2022-01-18 18:27:17 -0300

    Creacion modulos y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    PaisModule
   ],
   providers: [],
   bootstrap: [AppComponent]
