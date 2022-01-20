757f47e84683b7ed02900d874d45aa756111e90f
JereA00 <jeremias_davidenco@hotmail.com>
2022-01-20 17:52:30 -0300

    Implementacion componente por capital

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, Input } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-pais-input',
@@ -6,15 +7,34 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styles: [
   ]
 })
-export class PaisInputComponent {
+export class PaisInputComponent implements OnInit {
 
-  @Output() onEnter : EventEmitter<string> = new EventEmitter();
+  @Output() onEnter    : EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce : EventEmitter<string> = new EventEmitter();
 
+  @Input() placeholder : string = '';
   termino : string = '';
+
+  debouncer: Subject<string> = new Subject();
 
   constructor() { }
 
+  ngOnInit() {
+    this.debouncer.pipe(
+      debounceTime(300)
+    )
+    .subscribe(
+      valor => {
+          this.onDebounce.emit(valor);
+      }
+    );
+  }
+
   buscar(){
     this.onEnter.emit(this.termino);
+  }
+
+  teclaPresionada(){
+    this.debouncer.next(this.termino);
   }
 }
