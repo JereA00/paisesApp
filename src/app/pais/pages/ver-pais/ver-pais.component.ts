7a296a031eddb0892825c039479e3743ccdd32bc
JereA00 <jeremias_davidenco@hotmail.com>
2022-01-21 17:49:41 -0300

    Error en ver-pais component

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+
+import { PaisService } from '../../services/pais.service';
+
+import { Country } from '../../interfaces/pais.interfaces';
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,9 +14,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  paisSelect!: Country;
+  tipo : any;
+
+  constructor(
+    private activatedRoute : ActivatedRoute,
+    private paisService: PaisService
+  ) { 
+  }
 
   ngOnInit(): void {
+
+    this.activatedRoute.params.pipe(
+      switchMap( ({id}) => this.paisService.getPaisPorCapital(id)),
+      tap( console.log )
+    )
+    .subscribe( pais => this.paisSelect = pais);
+
+    // this.activatedRoute.params
+    // .subscribe( ({id}) => {
+
+    //    this.paisService.getPaisPorCapital(id)
+    //    .subscribe(pais => {
+    //      //this.paisSelect = pais;
+    //      console.log(pais);
+    //      this.paisSelect = pais;
+    //    }); 
+
+    // })   
+    
   }
 
 }
